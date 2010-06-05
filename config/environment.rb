5c064033ebbaf6525c4fe1f73acc51b5cc1e528f
Robin Wilson <r.t.wilson@rmplc.co.uk>
2010-06-05 14:27:54 -0700

    Froze rails and added RSpec files

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+ENV['GEM_PATH'] = "/home/robintw/.gems:/usr/lib/ruby/gems/1.8"
+
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,6 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  
+  
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
