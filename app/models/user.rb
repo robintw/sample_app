d84350869000dbdc1e9c93bc0de406fab02dbdbf
Robin Wilson <r.t.wilson@rmplc.co.uk>
2010-06-12 12:06:36 -0700

    Annotated models

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+# Schema version: 20100612182424
+#
+# Table name: users
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100604163947) do
+ActiveRecord::Schema.define(:version => 20100612182424) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
