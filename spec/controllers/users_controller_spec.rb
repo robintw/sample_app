90c9064402e9740505e310e197c73e402b9b24cf
Robin Wilson <r.t.wilson@rmplc.co.uk>
2010-06-12 19:00:39 -0700

    User model with passwords and user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require 'factory_girl'
+Factory.find_definitions
 
 describe UsersController do
 	integrate_views
@@ -14,4 +16,35 @@ describe UsersController do
     	response.should have_tag("title", /Sign up/)
     end
   end
+  
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
+      # Arrange for User.find(params[:id]) to find the right user.
+      User.stub!(:find, @user.id).and_return(@user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_tag("title", /#{@user.name}/)
+    end
+
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_tag("h2", /#{@user.name}/)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_tag("h2>img", :class => "gravatar")
+    end
+
+  end
+
 end
